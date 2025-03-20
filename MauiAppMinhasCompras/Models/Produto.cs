2f79430ffdf2bca51717aa94ff5bc9bbb7c2cf87
Carlos <Carlos@DESKTOP-DSKCT41>
2025-03-20 19:44:25 -0300

    Agenda 05

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,45 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao { 
+            get => _descricao;
+            set
+            {
+                if(value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição!");
+                }
+                _descricao = value;
+            }
+        }
+        public double Quantidade { 
+            get => _quantidade;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha com quantidade diferente de 0!");
+                }
+                _quantidade = value;
+            }
+        }
+        public double Preco { 
+            get => _preco; 
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha com preço diferente de 0!");
+                }
+                _preco = value;
+            }
+        }
 
         public double Total { get => Quantidade * Preco; }
     }
